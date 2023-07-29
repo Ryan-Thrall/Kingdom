1e75978931e1d3e605f28da6b10dcc2b35376dd2
Ryan-Thrall <ryanthrall04@gmail.com>
2023-07-29 12:03:29 -0600

    Setup Game Constructors

--- Kingdom/Controllers/GamesController.cs
+++ Kingdom/Controllers/GamesController.cs
@@ -5,5 +5,12 @@ namespace Kingdom.Controllers;
 
 public class GamesController : ControllerBase
 {
-  // private readonly GamesService _gamesServ;
+  private readonly GamesService _gamesServ;
+  private readonly Auth0Provider _auth0;
+
+  public GamesController(Auth0Provider auth0, GamesService gamesServ)
+  {
+    _auth0 = auth0;
+    _gamesServ = gamesServ;
+  }
 }
--- Kingdom/Services/GamesService.cs
+++ Kingdom/Services/GamesService.cs
@@ -2,5 +2,10 @@ namespace Kingdom.Services;
 
 public class GamesService
 {
+  private readonly GamesRepository _gamesRepo;
 
+  public GamesService(GamesRepository gamesRepo)
+  {
+    _gamesRepo = gamesRepo;
+  }
 }
